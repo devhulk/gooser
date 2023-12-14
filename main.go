10b633ed115245090fb5745f349ed5153bf5d230
devhulk <gry2big93@gmail.com>
2023-12-13 21:19:06 -0500

    added download and return map of json data

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
 const file_uri = "https://raw.githubusercontent.com/WebBreacher/WhatsMyName/main/wmn-data.json"
+const file_name = "wmn-data.json"
 
-func getSiteMap() (*os.File, error) {
+func getSiteMap() (map[string]interface{}, error) {
 
-	out, err := os.Create("wmn-data.json")
+	out, err := os.Create(file_name)
 	if err != nil {
 		return nil, err
 	}
@@ -22,7 +25,6 @@ func getSiteMap() (*os.File, error) {
 	defer out.Close()
 
 	resp, err := http.Get(file_uri)
-
 	if err != nil {
 		return nil, err
 	}
@@ -30,12 +32,29 @@ func getSiteMap() (*os.File, error) {
 	defer resp.Body.Close()
 
 	_, err2 := io.Copy(out, resp.Body)
-
 	if err2 != nil {
 		return nil, err
 	}
 
-	return out, nil
+	out.Close()
+
+	v, err := os.Open(file_name)
+	if err != nil {
+		return nil, err
+	}
+	defer v.Close()
+
+	byteValue, _ := ioutil.ReadAll(v)
+
+	var result map[string]interface{}
+
+	err3 := json.Unmarshal([]byte(byteValue), &result)
+	if err3 != nil {
+		return nil, err
+	}
+
+	return result, nil
+
 }
 
 func main() {
@@ -44,10 +63,14 @@ func main() {
 		if v == "" {
 			return errors.New("you didn't pass a value you dirty goose")
 		}
-		_, err := getSiteMap()
+		result, err := getSiteMap()
 		if err != nil {
 			log.Fatalln(err)
 		}
+
+		fmt.Println(result["categories"])
+		fmt.Println(result["sites"])
+
 		return nil
 	})
 
